3ecfc2560b65af13032170545afda8c653a4b436
shindler7 <barmichev@gmail.com>
2021-03-07 15:22:45 +0300

    first commit

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Учебный основной модуль.
+"""
+
+
+def main():
+    """
+    Стартовая функция.
+    """
+
+    print('Welcome to the Brain Games!')
+
+
+if __name__ == '__main__':
+    main()
